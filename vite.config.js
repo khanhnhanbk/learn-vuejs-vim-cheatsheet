52ceadb8778278fd2f91794693fc83ad07a3efa4
khanhnhanbk <nhannguyenqna2002@gmail.com>
2024-09-22 08:43:49 +0700

    build: prepare to build and deploy to github page

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/learn-vuejs-vim-cheatsheet/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
